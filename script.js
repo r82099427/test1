07e21056b3beadf2ac24a62153cac6bae12800e8
r82099427 <120941527+r82099427@users.noreply.github.com>
2022-12-28 00:05:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ $owl.children().each( function( index ) {
 });
 
 $owl.owlCarousel({
+  animateOut: 'slideOutDown',
+  animateIn: 'flipInX',
+  nav: true,
   center: true,
   loop: true,
+  merge:true,
+  URLhashListener:true,
+  autoplayHoverPause:true,
+  startPosition: 'URLHash',
   items: 6,
 });
 
@@ -14,4 +21,7 @@ $(document).on('click', '.owl-item>div', function() {
   // see https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html#to-owl-carousel
   var $speed = 300;  // in ms
   $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+  //$owl.trigger('')
 });
+
+
